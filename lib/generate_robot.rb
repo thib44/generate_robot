af2b45c7003ea5245085d0de89467760ef07d7f7
thibault finot <thibault.finot@gmail.com>
2016-03-16 15:26:35 +0100

    rails new gem for generate the robot.txt file

---
+++ lib/generate_robot.rb
@@ -0,0 +1,5 @@
+require "generate_robot/version"
+
+module GenerateRobot
+  # Your code goes here...
+end
---
+++ spec/generate_robot_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe GenerateRobot do
+  it 'has a version number' do
+    expect(GenerateRobot::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
